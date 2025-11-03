ad3a18d7a83361dc1ef9561c01f8e0dfeec047c8
jfoewifjweoifj <foweifowef9320r9howf>
2025-11-03 07:46:47 +0000

    update

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -296,7 +296,7 @@ def main():
     )
 
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.16298
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=0.97824
     )
 
     args = parser.parse_args()
