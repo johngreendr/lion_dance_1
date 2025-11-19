470404df693f729a0d23cb02333ddc17f6fe9266
John <johngreendr@gmail.com>
2025-11-19 01:27:08 +0000

    update

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -296,7 +296,7 @@ def main():
     )
 
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=0.96614
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=0.94618
     )
 
     args = parser.parse_args()
