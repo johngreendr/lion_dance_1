1ac76ca3776ece91596d7e16bc25e7f8a51934a2
John <johngreendr@gmail.com>
2026-03-03 23:38:03 +0000

    r5

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -241,7 +241,7 @@ def delete_poor_checkpoints(train_runs: list[dict]):
 
 
 def get_log_scale(task_type: str):
-    rank = 4
+    rank = 5
     reg_value = 0.27 * rank / 12
     log_scale_map = {
         TaskType.INSTRUCTTEXTTASK.value: 0.18 + reg_value,
@@ -298,7 +298,7 @@ def main():
     )
 
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.165418
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.199624
     )
 
     args = parser.parse_args()
